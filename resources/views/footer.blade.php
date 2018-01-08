e42250df9d18c488546a34522f80045813182fa3
Elbin <elbinfr.88@gmail.com>
2018-01-08 17:31:54 +0000

    actualizacion de footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,7 +4,7 @@
             <!-- copyright -->
             <div class="col-sm-12">
                 <h5><i class="fa fa-copyright text-theme-primary"></i> 
-                    2016 - Creado por <a href="http://elbinfr.esy.es/" target="_blank">Elbin Flores</a>
+                    2016 - Creado por <a href="http://www.nubedeideas.pe/elbin.flores" target="_blank">Elbin Flores</a>
                 </h5>
             </div>            
         </div>
